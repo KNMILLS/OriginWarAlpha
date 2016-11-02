3e503d22d75528808b6dbc282b219a781587c7cd
Evan Hitchings <ehitchings77@gmail.com>
2016-11-02 16:47:32 -0400

    Player singleton

--- core/src/io/zipcoder/Player.java
+++ core/src/io/zipcoder/Player.java
@@ -5,11 +5,28 @@ import squidpony.squidmath.Coord;
 public class Player extends Creature{
 
     private Coord position;
+    private static Player instance;
+    private double id;
 
-    public Player(Coord position) {
-        this.position = position;
+
+    private Player(){
+        id = Math.random();
+        Player.instance = this;
     }
-    public Player(){
+
+    public static Player getPlayer(){
+        if(Player.instance == null){
+            return new Player();
+        }
+        return instance;
+    }
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
     }
 
     public Coord getPosition() {
