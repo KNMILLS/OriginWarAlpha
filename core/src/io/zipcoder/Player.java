11de686ccac44e245ddb6b5a8d457d3664a9c364
Evan Hitchings <evanhitchings@Evan-ZCW-11.local>
2016-11-03 16:58:43 -0400

    field of view and lighting and health and turns taken

--- core/src/io/zipcoder/Player.java
+++ core/src/io/zipcoder/Player.java
@@ -5,9 +5,14 @@ import squidpony.squidmath.Coord;
 public class Player extends Creature{
 
     private static Player instance;
+    private int turns;
+    private int health;
+
 
     private Player(){
         setId(Math.random());
+        this.turns = 0;
+        this.health = 100;
         Player.instance = this;
     }
     public static Player getPlayer(){
@@ -17,5 +22,33 @@ public class Player extends Creature{
         return instance;
     }
 
+    @Override
+    public void setPosition(Coord position){
+        super.setPosition(position);
+        this.incrementTurn();
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public void setTurns(int turns) {
+        this.turns = turns;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void incrementTurn(){
+        this.turns += 1;
+        if(this.turns % 5 == 0){
+            this.health -= 1;
+        }
+    }
 
 }
