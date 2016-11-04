00edb708d232cd98ef8e412260facd9724e95481
Ken Ragonese <ken.ragonese@gmail.com>
2016-11-04 12:10:41 -0400

    move health from player to creature and add basic robot class

---
+++ core/src/io/zipcoder/Robot.java
@@ -0,0 +1,12 @@
+package io.zipcoder;
+
+/**
+ * Created by kenragonese on 11/4/16.
+ */
+public class Robot extends Creature{
+    Robot(){
+        setId(Math.random());
+        setHealth(100);
+    }
+
+}
