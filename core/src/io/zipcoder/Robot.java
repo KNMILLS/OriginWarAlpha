502bdcd175425de15b304c8d0e48c0e0b439ca51
Ken Ragonese <ken.ragonese@gmail.com>
2016-11-09 15:56:33 -0500

    add audio and update player/enemy/robot class

--- core/src/io/zipcoder/Robot.java
+++ core/src/io/zipcoder/Robot.java
@@ -3,10 +3,11 @@ package io.zipcoder;
 /**
  * Created by kenragonese on 11/4/16.
  */
-public class Robot extends Creature{
+public class Robot extends Enemy{
     Robot(){
-        setId(Math.random());
-        setHealth(100);
+        super();
+        this.setHealth(10);
+        this.setDamage(1);
     }
 
 }
